0ef33cfa2596884a274fa6f9127c96ad9f2ee899
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-17 13:40:05 +0100

    Add task

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+
+/*Задача 1
+Допишите программу из классной работы
+(см. файлы Account.java и MainAccounts.java).
+Необходимо создать собственное исключение InvalidEmail.
+При попытке создать Account нужно выбрасывать собственное исключение
+ если в переданном адресе электронной почты нет символа '@'.*/
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
